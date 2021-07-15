e2e398c945dc492e39f72f8cc7fccdda17f441ea
Amol Ambkar <amolambkar00@gmail.com>
2021-07-15 14:53:31 +0530

    wiki summary

---
+++ Wikipedia Summary/wiki_summary.py
@@ -0,0 +1,14 @@
+'''
+Generate Wiki Summary using Python
+Author: Amol Ambkar
+'''
+
+#import the necessary pywhatkit module!
+import pywhatkit as kt
+
+print('Lets Generate Wiki Summary!\n')
+
+topic = "python"
+
+#call the method
+kt.info(topic,lines=3)
--- Youtube Video Downloader/downloader.py
+++ Youtube Video Downloader/downloader.py
@@ -2,3 +2,21 @@
 # author - Amol Ambkar
 # Program - Download Youtube video using python
 #
+
+from pytube import YouTube
+import os
+
+link="https://www.youtube.com/watch?v=KsCiH6aYZd8"
+
+try:
+	# object creation using YouTube
+	# which was imported in the beginning
+	yt = YouTube(link)
+except:
+	print("Connection Error") 
+
+print(yt.title)
+stream = yt.streams.filter().first()
+out_file = stream.download()
+
+print(yt.title + " has been successfully downloaded.")
